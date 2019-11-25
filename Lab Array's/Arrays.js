842e0048994d35a839f4ec81a1de7e0e900a3ab2
happycooldude <pieterhuisman2000@gmail.com>
2019-11-25 14:47:51 +0100

    verder gewerkt aan de labs

--- Lab Array's/Arrays.js
+++ Lab Array's/Arrays.js
@@ -1,21 +1,95 @@
 var week = ["Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag", "Zondag"];
 
 document.getElementById("week").innerHTML = week;
+
 document.getElementById("werkdagen").innerHTML = week [0] + week [1] + week [2] + week [3] + week [4];
+
 document.getElementById("weekend").innerHTML = week [5] + week [6];
+
 omkeren();
-document.getElementById("omgekeerd").innerHTML = week;
+
+document.getElementById("dagen omgekeerd").innerHTML = week;
+
 omkeren();
+
 document.getElementById("werkdagenomgekeerd").innerHTML = week [4] + week [3] + week [2] + week [1] + week [0];
+
 document.getElementById("weekendomgekeerd").innerHTML = week [5] + week [6];
 
 function omkeren(){
     week.reverse();
 }
 
-var namen = [];
-while(userInput != stop){
-    var userInput = prompt("Welke naam wilt u toevoegen?");
-    namen.push(userinput);
+var userInput, namen = [];
+while(userInput !== "n"){
+    namen.push(userInput = prompt("Welke naam wilt u toevoegen?"));
+    userInput = prompt("Wilt u nog een naam aan de lijst toevoegen? y/n");
 }
+
 document.getElementById("namen").innerHTML = namen;
+
+function omkeernamen(){
+    namen.reverse();
+}
+
+omkeernamen();
+document.getElementById("namen omgekeerd").innerHTML = namen;
+omkeernamen();
+
+var arrayEen = [1,2,3,4,5,6,7,8,9,10];
+var arrayTwee = [2,4,6,8,10,12,14,16,18,20];
+
+// optellen(arrayEen, arraytwee);
+for(var i = 0; i < arrayEen.length; i++){
+    document.getElementById("optellen").innerHTML += arrayEen[i] + ' + ' + arrayTwee[i] + ' = ' + (arrayEen[i] + arrayTwee[i]) +  '<br/>';
+}
+
+// aftrekken(arrayTwee, arrayEen);
+for(var i = 0; i < arrayEen.length; i++){
+    document.getElementById("aftrekken").innerHTML += arrayTwee[i] + " - " + arrayEen[i] + " = " + (arrayTwee[i] - arrayEen[i]) + "<br/>";
+}
+
+// vermenigvuldigen(arrayEen, arrayTwee);
+for(var i = 0; i < arrayEen.length; i++){
+    document.getElementById("vermedigvuldigen").innerHTML += arrayEen[i] + " * " + arrayTwee[i] + " = " + (arrayEen[i] * arrayTwee[i] + "<br/>");
+}
+
+// delen(arrayTwee, arrayEen);
+for(var i = 0; i < arrayEen.length; i++){
+    document.getElementById("delen").innerHTML += arrayTwee[i] + " / " + arrayEen[i] + " = " + (arrayTwee[i] / arrayEen[i] + "<br/>");
+}
+
+var arraytafels = [2,4,6,8];
+
+//de tafels van 2, 4, 6, 8,
+for(var i = 0; i <arrayEen.length; i++){
+    document.getElementById("2").innerHTML += arrayEen[i] + " * " + arraytafels[0] + " = " + (arrayEen[i] * arraytafels[0] + "<br/>");
+}
+
+for(var i = 0; i <arrayEen.length; i++){
+    document.getElementById("4").innerHTML += arrayEen[i] + " * " + arraytafels[1] + " = " + (arrayEen[i] * arraytafels[1] + "<br/>");
+}
+
+for(var i = 0; i <arrayEen.length; i++){
+    document.getElementById("6").innerHTML += arrayEen[i] + " * " + arraytafels[2] + " = " + (arrayEen[i] * arraytafels[2] + "<br/>");
+}
+
+for(var i = 0; i <arrayEen.length; i++){
+    document.getElementById("8").innerHTML += arrayEen[i] + " * " + arraytafels[3] + " = " + (arrayEen[i] * arraytafels[3] + "<br/>");
+}
+
+// var inputgetal, getal = [];
+
+// getal.push(inputgetal = prompt("voer een getal in"));
+// for(var i = 0; i <inputgetal; i++){
+
+// document.getElementById("tafeloplopend").innerHTML = getal + "<br/>";
+// }
+
+var num = "";
+var size = prompt("Enter the size of the pyramid");
+for(var i=1; i<=size; i++)
+{
+  num = num + i
+  console.log(num);
+}
